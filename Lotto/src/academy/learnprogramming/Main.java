23f24d2a961ee7bbe127452e6fc3c91e471e9045
Adam Bogusławski <45942135+aboguslawski@users.noreply.github.com>
2020-01-20 22:42:13 +0100

    Add files via upload

---
+++ Lotto/src/academy/learnprogramming/Main.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame a = new Board();
+        a.setSize(1200,600);
+        a.setLocation(100,100);
+        a.setVisible(true);
+    }
+}
